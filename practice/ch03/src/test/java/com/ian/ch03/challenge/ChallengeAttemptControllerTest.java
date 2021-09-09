223a699943c5248899d96190ea5b28ffe43e9cb7
YING HE <yinghe@yings-mbp.home>
2021-09-09 11:45:55 +1000

    update ch03

--- practice/ch03/src/test/java/com/ian/ch03/challenge/ChallengeAttemptControllerTest.java
+++ practice/ch03/src/test/java/com/ian/ch03/challenge/ChallengeAttemptControllerTest.java
@@ -8,11 +8,13 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -43,9 +45,35 @@ class ChallengeAttemptControllerTest {
         given(challengeService.verifyAttempt(eq(attemptDTO))).willReturn(expectedResponse);
 
         // when
+        var requestBody = jsonRequestAttempt.write(attemptDTO).getJson();
+        System.out.println("requestBody:");
+        System.out.println(requestBody);
         MockHttpServletResponse response = mvc.perform(
-                post("/attempts").contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonRequestAttempt.write(attemptDTO).getJson())
-        )
+                        post("/attempts").contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
+                .andReturn().getResponse();
+
+        // then
+        var expectedResponseBody = jsonResultAttempt.write(expectedResponse).getJson();
+        System.out.println("expectedResponseBody:");
+        System.out.println(expectedResponseBody);
+        then(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        then(response.getContentAsString()).isEqualTo(expectedResponseBody);
+    }
+
+    @Test
+    void post_invalid_result() throws Exception {
+        // given an attempt with invalid input data
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(2000, -70, "john", 1);
+
+        // when
+        var requestBody = jsonRequestAttempt.write(attemptDTO).getJson();
+        var resp = mvc
+                .perform(post("/attempts").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andReturn()
+                .getResponse();
+
+        // then
+        then(resp.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
